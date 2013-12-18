49c0b9616305e7e801c5f2c5cea0da51e92edc06
Garrett Clay <clayg@onid.orst.edu>
2013-12-18 11:11:28 -0800

    Updated several functions. Specifically SPI_sendbyte() and SPI_sendpacket(). Cleaned up several functions that had noticable errors. Updated hardware configuration to at90usb but now it needs to be updated to mega128. Most pins should stay the same. Need to verify init functions like pwm and ddr. I expect they won't need to change.

--- epd/main.c
+++ epd/main.c
@@ -1,23 +1,28 @@
 /* main.c
- * author: Tuan
+ * authors: Tuan & Garrett
  * date: 11/21/13
- * mcu: at90usb1287
+ * mcu: mega128
  * brief: test code for 2.0" TFT EPD Panel SG020AS0T1.
  * Objective to construct an image on the EPD using SPI
  * and determine if EPD is easy to use and EPD should be 
  * implemented in project.
  *
  * hardware:
- * - Buttons	PA0-PA7
- * - SPI_CS	PB0
- * - SPI_SCK	PB1
- * - SPI_MOSI	PB2
- * - SPI_MISO	PB3
+ * - Buttons	PA0-PA7		input to MCU                    
+ * - SPI_CS	PB0		output from MCU
+ * - SPI_SCK	PB1		output from MCU
+ * - SPI_MOSI	PB2		output from MCU
+ * - SPI_MISO	PB3		input to MCU
+ * - BORDER_PIN	PB4		output from MCU
+ * - PWM_OC1A	PB5		output from MCU
+ * - vcc	PB6		output from MCU
+ * - BUSY_PIN	PB7		input to MCU
+ *
  * - LEDs	PC0-PC7
  * - USART_RX	PD2
  * - USART_TX	PD3
- * - PWM_OC1A	PB5
- *
+ * - RESET_PIN	PD4
+ * - ID pin	gnd
  */
 
 /* PLAN OF ATTACK:
@@ -36,6 +41,8 @@
 //includes
 #include<avr/io.h>
 #include<util/delay.h>
+#include<stdlib.h>
+#include<stdio.h>
 
 
 //global variables
@@ -48,23 +55,25 @@ uint8_t test_buffer_nth[5000];
 
 
 //hardware macros
-#define CS_PIN		0
-#define SCK_PIN		1
-#define MOSI_PIN	2
-#define MISO_PIN	3
-#define RX_PIN		2	
-#define TX_PIN		3
-#define PWM_PIN		5
+#define CS_PIN		0	//PB0
+#define SCK_PIN		1	//PB1
+#define MOSI_PIN	2	//PB2
+#define MISO_PIN	3	//PB3
+#define BORDER_PIN	4	//PB4
+#define PWM_PIN		5	//PB5
+#define vcc		6	//PB6
+#define BUSY_PIN	7	//PB7
+
+#define RX_PIN		2	//PD2
+#define TX_PIN		3	//PD3
+#define RESET_PIN	4	//PD4
 
 //#define CS_PIN		0	//delete line, repeated
-#define BUSY_PIN	1
-#define ID_PIN		2
-#define RESET_PIN	3
-#define BORDER_PIN	4
+//#define ID_PIN		2	//TODO ask Tuan what this is for?
 
 //protocol macros
-#define REG_HEADER	0x70
-#define DATA_HEADER	0x72
+#define REG_HEADER	0x70	//send before sending reg. index
+#define DATA_HEADER	0x72	//send before sending reg. data
 
 #define PWM_DISABLE()	TCCR1B &= ~(7<<CS10)
 #define PWM_ENABLE()	TCCR1B |= (2<<CS10)
@@ -72,6 +81,7 @@ uint8_t test_buffer_nth[5000];
 //set up the data direction registers (1=output, 0=input)
 void setup_ddr(){
 	//buttons are inputs
+	//1 = output, 0 = input
 	DDRA = 0x00;
 	//output spi
 	DDRB |= (1<<CS_PIN) | (1<<SCK_PIN) | (1<<MOSI_PIN);
@@ -113,16 +123,20 @@ void setup_pwm(){
 	//Set waveform mode
 	//mode 4 pwm CTC, top OCR1A, update at bottom
 	//TCCR1A |= (1<<WGM10);
-	TCCR1B |= (1<<WGM12);		//CTC mode? not PWM?
+	TCCR1B |= (1<<WGM12);		//making our own pwm
 
 	//Set timer clock sel clk/8
+	//16mhz / 8 = 2mhz
+	//2mhz / (OCR1A = 10) = 200khz
 	TCCR1B &= ~(7<<CS10);    	
 	TCCR1B |= (2<<CS10);		
                                         
+	//set compare value
+	OCR1A = 10;
 
 	//enable timer interrupt
-	TIMSK1 = (1<<ICIE1);
-		
+	TIMSK1 = (1<<OCIE1A);    
+	
 	return;
 }
 
@@ -137,27 +151,93 @@ void SPI_sendbyte(uint8_t data){
 	return;
 }
 
-//Sending SPI packet of data
-//delay  >= 10us between packet
-void SPI_sendpacket(uint8_t header, uint8_t *data){
-	//pull CS_PIN low
+//Sending SPI byte of data
+//1. set CS low
+//2. send REG_HEADER
+//3. send reg_index
+//4. toggle CS high then low
+//5. send DATA_HEADER
+//6. send data
+//7. set CS high
+void SPI_sendbyte(uint8_t reg_index, uint8_t data){
+	//1. pull CS_PIN low
 	PORTB &= ~(1<<CS_PIN);
     
-	//send header for data
-	SPI_sendbyte(header);
+	//2. send header for data
+	SPI_sendbyte(REG_HEADER);
 
-	//send data
-	while(*data){       	//what if no NULL after data array?
-		SPI_sendbyte(*data);
-		data++;
-	}
+	//3. send reg_index
+	SPI_sendbyte(reg_index);
 
-	//set CS_PIN high
+	//4. toggle CS
 	PORTB |= (1<<CS_PIN);
+	_delay_us(10);
+	PORTB &= ~(1<<CS_PIN);
+
+	//5. send DATA_HEADER
+	SPI_sendbyte(DATA_HEADER);
+
+	//6. send data
+	SPI_sendbyte(data);
+
+	//7. set CS_PIN high
+	PORTB |= (1<<CS_PIN);
+	_delay_us(10);		//wait 10us between packets
 
 	return;
 }
 
+//delete if not using pointers to send data
+//use for filling memory sequentially
+/*
+void fill_pointer(uint8_t *data_ptr, uint64_t data, uint8_t num_bytes){
+	uint8_t i;
+	for(i=0;i<num_bytes;i++){
+		*data_ptr = ((data & (0xFF<<(i*8)))>>(i*8));
+		data_ptr++;
+	}
+	*data_ptr = NULL;
+}                
+*/
+
+//Sending SPI packet of data
+//1. set CS low
+//2. send REG_HEADER
+//3. send reg_index
+//4. toggle CS high then low
+//5. send DATA_HEADER
+//6. send data
+//7. set CS high
+void SPI_sendpacket(uint8_t reg_index, uint64_t data, uint8_t num_bytes){
+	//1. pull CS_PIN low
+	PORTB &= ~(1<<CS_PIN);
+    
+	//2. send header for data
+	SPI_sendbyte(REG_HEADER);
+
+	//3. send reg_index
+	SPI_sendbyte(reg_index);
+
+	//4. toggle CS
+	PORTB |= (1<<CS_PIN);
+	_delay_us(10);
+	PORTB &= ~(1<<CS_PIN);
+
+	//5. send DATA_HEADER
+	SPI_sendbyte(DATA_HEADER);
+
+	//6. send data
+	//TODO review with Tuan.
+	for(i=0;i<num_bytes;i++)
+		SPI_sendbyte((data & (0xFF<<(i*8)))>>(i*8));
+	}
+
+	//7. set CS_PIN high
+	PORTB |= (1<<CS_PIN);
+	_delay_us(10);		//wait 10us between packets
+
+	return;
+} 
 
 
 //made for testing
@@ -165,30 +245,32 @@ void SPI_sendpacket(uint8_t header, uint8_t *data){
 //2" display 200 x 96 pixel
 void fill_image_buff(uint8_t *buffer){
 	uint8_t data_counter = 0;
-	uint8_t pixel_data = 0xff;
+	uint8_t pixel_data = 0xFF; //TODO are pixels 1&0 or byte value?
 	uint8_t scan_counter = 0;
 	uint8_t line_counter= 0;
 
-	//for 2" EPD resolution 200 x 97
+	//for 2" EPD resolution 200 x 96
 	//draw 97 lines with 200 pixels
-	for(line_counter = 0; line_counter<97; line_counter++){
+	for(line_counter = 0; line_counter<96; line_counter++){
 		//set the even bits of image
 		for(data_counter = 0; data_counter<25; data_counter++){
 			*buffer = pixel_data;
 			buffer++;
 		}
 	
-		//set the can bits
+		//set the scan bits
 		for(scan_counter = 0; scan_counter<24 ; scan_counter++){
-			*buffer= 0xff;
+			*buffer= 0xFF;
 			buffer++;
 		}
 
 		//set the odd bits of image
-		for(data_counter =25; data_counter<50; data_counter++){
+		for(data_counter = 25; data_counter<50; data_counter++){
 			*buffer = pixel_data;
 			buffer++;
 		}
+		
+		//TODO why didn't we fill to 200? stopped at 99
 
 		//blank byte
 		*buffer = 0x00;
@@ -198,37 +280,37 @@ void fill_image_buff(uint8_t *buffer){
 
 	return;
 }
-
+               
 //powers on COG
+//0. start with vcc/vdd, /reset, /cs, border, SI, SCLK = 0
+//1. start pwm toggling
+//2. vcc/vdd voltage > 2.7v 
+//3. /cs = 1
+//4. border = 1
+//5. toggle reset 
 void startup_cog(){
-	//Enable PWM
+	//1. Enable PWM
 	PWM_ENABLE();
-	_delay_us(5);
-	//Supply voltage
-	
-	//toggle PWM for >= 10ms 
-	_delay_us(10);
+	_delay_ms(5);
 
-	//Set /CS = 1
-	PORTF |= (1<<CS_PIN); 	
-	//Set BORDER = 1
-	PORTF |= (1<<BORDER_PIN);	
-	//Set /RESET = 1
-	PORTF |= (1<<RESET_PIN);
-	//toggle PWM for >= 5ms
-	_delay_us(5);
-	
-	//Clear /RESET=0
-	PORTF &= ~(1<<RESET_PIN);
-	//toggle PWM for >= 5ms
-	_delay_us(5);
-	
-	// Set /RESET = 1
-	PORTF |= (1<<RESET_PIN);
-	
-	//toggle PWM for >= 5ms
-	_delay_us(5);
-	
+	//2. Supply voltage
+	PORTB |= (1<vcc);
+	_delay_ms(10);
+
+	//3. Set /cs = 1
+	PORTB |= (1<<CS_PIN); 	
+
+	//4. Set BORDER = 1
+	PORTB |= (1<<BORDER_PIN);	
+
+	//5. toggle reset
+	PORTD |= (1<<RESET_PIN);
+	_delay_ms(5);
+	PORTD &= ~(1<<RESET_PIN);
+        _delay_ms(5);
+	PORTD |= (1<<RESET_PIN);
+	_delay_ms(5);
+
 	//POWER is one and the next step is to initialize COG
 	return; 
 
@@ -236,6 +318,10 @@ void startup_cog(){
 
 
 void init_cog(){
+	//SPI_sendpacket(uint8_t reg_index, uint8_t *data)
+	//pass data as an address. ie &data
+	uint8_t data;
+
 	//Check if COG is busy
 	while(BUSY_PIN);
 
